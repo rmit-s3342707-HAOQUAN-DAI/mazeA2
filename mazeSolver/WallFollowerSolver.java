7b4b668aff2db80e81f54ac0d1b041517a1a4eb7
rmit-s3342707-HAOQUAN-DAI <s3342707@student.rmit.edu.au>
2017-10-11 22:57:19 +1100

    11/10

--- mazeSolver/WallFollowerSolver.java
+++ mazeSolver/WallFollowerSolver.java
@@ -1,5 +1,9 @@
 package mazeSolver;
 
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.ArrayList;
+import maze.Cell;
 import maze.Maze;
 
 /**
@@ -8,13 +12,26 @@ import maze.Maze;
 
 public class WallFollowerSolver implements MazeSolver {
 	
+//https://github.com/briankcook/Maze/blob/master/mazemaker/maze/solvers/WallFollower.java
+
+	
+//https://github.com/green131/maze-solver/blob/master/src/src/MazeSolver.java
+	
+	int[][] solution;
+	private int[][] visited; // track visited area in maze
+	private int[][] location;  // track current position in maze
+	
+	
 	@Override
 	public void solveMaze(Maze maze) {
 		// TODO Auto-generated method stub
-        
+		
+	
 	} // end of solveMaze()
-    
-    
+	
+	
+	
+
 	@Override
 	public boolean isSolved() {
 		// TODO Auto-generated method stub
@@ -29,3 +46,34 @@ public class WallFollowerSolver implements MazeSolver {
 	} // end of cellsExplored()
 
 } // end of class WallFollowerSolver
+
+//------------------example 1 ends----------------------------
+
+
+//-------------------------example 2-----------------------------------
+
+//public class WallFollowerSolver implements MazeSolver {
+//	
+//	
+//	
+//	@Override
+//	public void solveMaze(Maze maze) {
+//		// TODO Auto-generated method stub
+//        
+//	} // end of solveMaze()
+//    
+//    
+//	@Override
+//	public boolean isSolved() {
+//		// TODO Auto-generated method stub
+//		return false;
+//	} // end if isSolved()
+//    
+//    
+//	@Override
+//	public int cellsExplored() {
+//		// TODO Auto-generated method stub
+//		return 0;
+//	} // end of cellsExplored()
+//
+//} // end of class WallFollowerSolver
